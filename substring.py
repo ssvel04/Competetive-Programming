68e8a3428eabbbe49660d9062f0e6b30d4bb715f
ssvel04 <37283919+ssvel04@users.noreply.github.com>
2018-12-23 10:37:07 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,7 +1,7 @@
 T = int(input())                  
-
 N = []
 S = []
+
 for i in range(0,T):   
     N.append(int(input()))
     S.append(input())
@@ -9,10 +9,7 @@ for i in range(0,T):
 for i in range(0,T):
     s = S[i]    
     n = N[i]
-    found = False
     for clen in range(n,-1,-1):
-        if(found):
-            break
-        for strt in range(0,n-clen):
-            end = clen+strt
+        for start in range(0,n-clen):
+            end = clen+start
             sub = s[strt:end+1]
