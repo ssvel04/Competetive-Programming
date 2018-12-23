3ab4d82fd2cae1acb1440dca980e29fc6d0a2242
ssvel04 <37283919+ssvel04@users.noreply.github.com>
2018-12-23 18:21:10 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -10,6 +10,6 @@ for i in range(0,T):
     s = S[i]    
     n = N[i]
     for clen in range(n,-1,-1):
-        for start in range(0,n-clen):
-            end = clen+start
-            sub = s[strt:end+1]
+        for start in range(0,n-clen+1):
+            end = start+clen
+            sub = s[strt:end]
